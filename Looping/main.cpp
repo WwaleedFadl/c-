9d30ec27f4feac9eb2a30c2f4a54a960b6684f12
waleed fadl <wwwaleedfadl@gmail.com>
2025-06-04 17:13:40 +0300

    adding range based looop

--- Looping/main.cpp
+++ Looping/main.cpp
@@ -3,21 +3,25 @@
 using namespace std;
 
 int main() {
-
   /*
   for (int i{1};i<=10;++i)
     cout << i << endl;
+
   for (int i{2};i<=10;i+=2)
     cout << i << endl;
+
   for (int i{10};i>0;i-=2) 
       cout << i << endl;
+
   for(int i{10} ; i <= 100 ; i+=10) {
     if(i % 15 == 0 )
       cout << i << endl;
   }
+
   for(int i{1},j{5}; i<=5;++i,++j){
     cout << i << " + " << j << " = " << i+j << endl;
   }
+
   for(int i {1};i<=100;++i) {
     cout << i; 
     if(i % 10 == 0 ) 
@@ -25,15 +29,35 @@ int main() {
     else 
       cout << " ";
   }
+
   for(int i {1};i<=100;++i) {
     cout << i << ((i % 10 == 0) ? "\n" : " ");
   }
-  */ 
+
   vector <int> nums{10,20,30,40,50};
   for(unsigned i {0} ; i < nums.size();++i) {
     cout << nums[i] << endl;
   }
-
+  */ 
+  // range based for loop
+  /*
+  int scores [] {100,90,97};
+  for (int score:scores) 
+    cout << score << endl;
   cout << endl;
+  double average_temp{};
+  double running_sum{};
+  int size{0};
+  for(auto temp:{44.3,22.3}){
+    running_sum += temp;
+    ++size;
+  }
+  average_temp = running_sum / size;
+  cout << average_temp << endl;
+  for(auto c:"Waleed") {
+    cout << c << endl;
+  }
+
+  */
   return 0;
 }
---
+++ rangeBasedForLoop/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector> 
+#include <iomanip>
+using namespace std; 
+
+int main(){
+  /*
+  int scores [] {10,20,30};
+  for(auto score :scores){
+    cout << score << endl;
+  }
+  vector<double> tempreatures{44.4,22.2};
+  double average_temp{};
+  double total{};
+
+  for(auto temp:tempreatures)
+    total += temp; 
+
+  cout << total << endl;
+
+  if(tempreatures.size() != 0) 
+    average_temp = total / tempreatures.size();
+
+  cout << fixed << setprecision(1);
+
+  cout << average_temp << endl;
+
+  for(auto val:{1,2,3,4,5})
+    cout << val << endl;
+
+  for(auto c:"Wa leeeeeeeeeeeeeeeeeeeeeed")
+    if(c != ' ')
+      cout << c;
+  for(auto c:"Wa leeee  eeeee  eee     eeeee    eeeeed")
+      if(c == ' ') 
+        cout << '\t';
+      else
+      cout << c;
+  */
+  cout << endl;
+  return 0;
+}
